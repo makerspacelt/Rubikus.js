9de1a91bbabbba569edec6bd6cc48f5ef467aa92
Karolis Grinkus <karolis@kartais.lt>
2013-08-15 20:12:53 +0300

    Create rubikus.js
    Starting with an extend utility and AMD module load

---
+++ rubikus.js
@@ -0,0 +1,50 @@
+/*
+ * Rubikus.js
+ *
+ * Copyright 2013, Karolis Grinkus - http://kartais.lt
+ *
+ * Github: http://github.com/
+ */
+
+(function (undefined) {
+    'use strict';
+    var Rubikus = Rubikus || function (userOpts) {
+        var settings = {
+                element: null
+            },
+
+            utils = {
+                extend: function (destination, source) {
+                    for (var property in source) {
+                        if (source[property] && source[property].constructor && source[property].constructor === Object) {
+                            destination[property] = destination[property] || {};
+                            utils.extend(destination[property], source[property]);
+                        } else {
+                            destination[property] = source[property];
+                        }
+                    }
+                    return destination;
+                }
+            },
+
+            init = function (opts) {
+                utils.extend(settings, opts);
+            };
+
+        init(userOpts);
+    };
+
+    if (typeof module !== 'undefined' && module.exports) {
+        module.exports = Rubikus;
+    }
+
+    if (typeof ender === 'undefined') {
+        this.Rubikus = Rubikus;
+    }
+
+    if (typeof define === "function" && define.amd) {
+        define('Rubikus', [], function () { 
+            return Rubikus; 
+        });
+    }
+}).call(this);
