e11b66f6f9099ee84e79b92684d200adc1634354
Karolis Grinkus <karolis@kartais.lt>
2013-08-15 23:38:50 +0300

    Add binary number padding utility

--- rubikus.js
+++ rubikus.js
@@ -10,7 +10,8 @@
     'use strict';
     var Rubikus = Rubikus || function (userOpts) {
         var settings = {
-                element: null
+                element: null,
+                hash: '358ed264d3b8a832b86bc4db745f182ef71bc65fd0a62dd5d760862fe1c3986b' // makerspace
             },
 
             utils = {
@@ -24,6 +25,20 @@
                         }
                     }
                     return destination;
+                },
+
+                pad: function (number, places) {
+                    number = number.toString();
+                    if (typeof places === 'undefined') {
+                        places = 4;
+                    }
+
+                    if (number.length >= places) {
+                        return number;
+                    }
+
+                    number = '0' + number;
+                    return utils.pad(number, places);
                 }
             },
 
